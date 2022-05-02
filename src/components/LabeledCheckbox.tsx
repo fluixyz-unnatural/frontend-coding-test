7a43a31469c8d9c7c10edf422c822114ff8803e9
fluixyz-unnatural <sudo3949@gmail.com>
2022-05-02 19:03:03 +0900

    add labeled checkbox component

--- src/components/LabeledCheckbox.tsx
+++ src/components/LabeledCheckbox.tsx
@@ -1,15 +1,16 @@
 import React, { ChangeEventHandler } from 'react'
 interface Props {
+  value: boolean
   label: string
   handleChange: ChangeEventHandler
 }
 export default function LabeledCheckbox(props: Props) {
   return (
-    <span>
+    <div>
       <label>
-        <input type="checkbox" onChange={props.handleChange}></input>
+        <input type="checkbox" checked={props.value} onChange={props.handleChange}></input>
         {props.label}
       </label>
-    </span>
+    </div>
   )
 }

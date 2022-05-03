2502e80d94a6cc30340e60c4eaa3e59d285204e9
fluixyz-unnatural <sudo3949@gmail.com>
2022-05-03 13:22:50 +0900

    add prefucture population chart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import PrefsCheckBox from './components/PrefsCheckbox'
-import { usePrefuctures } from './hooks/areas'
+import PrefsPopulationChart from './components/PrefsPopulationChart'
+import { usePrefucturePopulation, usePrefuctures } from './hooks/areas'
 import './style.css'
-import { CheckedPrefucture } from './types/areas.d'
+import { CheckedPrefucture, Prefucture } from './types/areas.d'
 
 function App() {
   const prefs = usePrefuctures()
-  const [checked, setChecked] = useState([] as Array<CheckedPrefucture>)
+  const [checked, setChecked] = useState<Array<CheckedPrefucture>>([])
+  const [display, setDisplay] = useState<Array<Prefucture>>([])
+  const populations = usePrefucturePopulation(display)
+  console.log('population', populations)
   useEffect(() => {
     setChecked(
       prefs.map((e) => ({
@@ -16,7 +20,12 @@ function App() {
     )
   }, [prefs])
   useEffect(() => {
-    console.log(checked.filter((elm) => elm.checked).map(elm=>elm.prefucture.prefName))
+    console.log(
+      checked.filter((elm) => elm.checked).map((elm) => elm.prefucture.prefName)
+    )
+    setDisplay(
+      checked.filter((elm) => elm.checked).map((elm) => elm.prefucture)
+    )
   }, [checked])
   return (
     <div>
@@ -34,6 +43,8 @@ function App() {
           return
         }}
       />
+      <PrefsPopulationChart populations={populations} />
+      {populations.map((elm) => elm.prefucture.prefName).join(', ')}
     </div>
   )
 }
